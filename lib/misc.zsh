f39a10860e8ddfbc87ada542479b8df895191417
Sam Davies <sampdavies@gmail.com>
2017-10-06 10:53:13 +0100

    Disable fucking slow paste plugin

--- lib/misc.zsh
+++ lib/misc.zsh
@@ -4,10 +4,10 @@ autoload -Uz is-at-least
 if [[ $ZSH_VERSION != 5.1.1 ]]; then
   for d in $fpath; do
   	if [[ -e "$d/url-quote-magic" ]]; then
-  		if is-at-least 5.1; then
-  			autoload -Uz bracketed-paste-magic
-  			zle -N bracketed-paste bracketed-paste-magic
-  		fi
+#  		if is-at-least 5.1; then
+#  			autoload -Uz bracketed-paste-magic
+#  			zle -N bracketed-paste bracketed-paste-magic
+#  		fi
   		autoload -Uz url-quote-magic
   		zle -N self-insert url-quote-magic
       break
